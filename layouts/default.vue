6b9b5f7f45174576fb216a664120283a324913de
damiakadiri <amosakadiri2@gmail.com>
2023-11-22 12:40:34 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">MUSE</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
